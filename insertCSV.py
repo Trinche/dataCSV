159554f5d7d1656c389810eed5f2bbfd04978a08
Trinche <m.trinchero11@gmail.com>
2017-05-23 17:55:06 +0200

    added Bootstrap and required controll

--- insertCSV.py
+++ insertCSV.py
@@ -1,5 +1,6 @@
 import csv
 from flask import Flask, render_template, flash, request
+from werkzeug.datastructures import MultiDict
 
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
@@ -11,12 +12,16 @@ app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 
 class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
-    surname = TextField('surname:', validators=[validators.required()])
-    job = TextField('job:', validators=[validators.required()])
-    company = TextField('company:', validators=[validators.required()])
-    city = TextField('city:', validators=[validators.required()])
-    country = TextField('country:', validators=[validators.required()])
+    name = StringField('Name:', validators=[validators.required()])
+    surname = StringField('surname:', validators=[validators.required()])
+    job = StringField('job:', validators=[validators.required()])
+    company = StringField('company:', validators=[validators.required()])
+    city = StringField('city:', validators=[validators.required()])
+    country = StringField('country:', validators=[validators.required()])
+
+    def reset(self):
+        blankData = MultiDict([('csrf', self.reset_csrf())])
+        self.process(blankData)
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -31,17 +36,16 @@ def hello():
         company = request.form['company']
         city = request.form['city']
         country = request.form['country']
-
         fieldnames = ['name', 'surname', 'job', 'company', 'city', 'country']
-
 
         if form.validate():
             with open('nameList.csv', 'a') as inFIle:
+                flash('Thanks for registration ' + name)
                 writer = csv.DictWriter(inFIle, fieldnames=fieldnames)
                 writer.writerow({'name': name, 'surname': surname, 'job': job, 'company': company, 'city': city,
                                  'country': country})
         else:
-            flash('All the form fields are required. ')
+            flash('Error: All the form fields are required. ')
 
     return render_template('form.html', form=form)
 
